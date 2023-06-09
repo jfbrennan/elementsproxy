22ae4e1ef45cc46ef2196f5ca016f840d3006522
Jordan Brennan <jordanbrennan@me.com>
2023-06-08 23:03:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 /**
  * Proxy object that uses `getElementById` to look up an element with an id matching the
  * accessed property name and stores the element as a property on the returned object.
- * @typedef {string} elementId
- * @returns {Object.<elementId, HTMLElement>}
+ * @returns {Object.<string, HTMLElement>}
  */
 export default function elementsProxy() {
   return new Proxy({}, {
